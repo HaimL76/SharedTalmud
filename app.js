02cbd37cf819f3442b00b50a36c50942d966056d
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-09-03 18:20:38 +0300

    3

--- app.js
+++ app.js
@@ -9,9 +9,42 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', (req, res) => {
-    res.send('kukuriku')
-})
+    var sql = require("mssql");
+
+    var Connection = require('tedious').Connection;
+
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'sa',
+        server: 'localhost',
+        database: 'SharedTalmud'
+    };
+
+    var connection = new Connection(config);
+
+    //console.log(JSON.stringify(connection));
+
+    connection.on('connect', function(err) {
+        // If no error, then good to proceed.  
+        console.log("Connected");
+
+        executeStatement1();
+    });
+
+    connection.connect();
+});
+
+function executeStatement1() {
+    console.log("lalaluliu");
+
+    request = new Request("insert into SharedTalmud.Comments (ResId, Row, Col) values(1, 1, 1));", function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
