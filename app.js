25f57d1bd9752cc5a848696dc2ab9d1b88c42fa0
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-09-05 22:04:31 +0300

    6

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    server: "localhost",
+    authentication: {
+        type: "default",
+        options: {
+            userName: "sa",
+            password: "sa",
+        }
+    },
+    database: "SharedTalmud"
+};
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -13,29 +25,7 @@ app.get('/', (req, res) => {
 
     const Connection = require('tedious').Connection;
 
-    // config for your database
-    let config0 = {
-        user: 'sa',
-        password: 'sa',
-        server: 'localhost',
-        database: 'SharedTalmud'
-    };
-
-    var config = {
-        server: "localhost",
-        authentication: {
-            type: "default",
-            options: {
-                userName: "sa",
-                password: "sa",
-            }
-        },
-        database: "SharedTalmud"
-    };
-
     var connection = new Connection(config);
-
-    //console.log(JSON.stringify(connection));
 
     connection.on('connect', function(err) {
         // If no error, then good to proceed.  
@@ -48,13 +38,41 @@ app.get('/', (req, res) => {
     connection.connect();
 });
 
-function executeStatement1(connection) {
-    var Request = require('tedious').Request
+app.post('/:row/:col', (req, res) => {
+    if (req && params in req)
+        console.log(JSON.stringify(req.params));
 
-    request = new Request("insert into [SharedTalmud].[dbo].[Comments] (ResId, Row, Col) values(1, 1, 1);", function(err) {
-        if (err) {
+    var sql = require("mssql");
+
+    const Connection = require('tedious').Connection;
+
+    var connection = new Connection(config);
+
+    connection.on('connect', function(err) {
+        // If no error, then good to proceed.  
+        if (err)
             console.log(JSON.stringify(err));
-        }
+
+        console.log(row);
+        console.log(col);
+
+        executeStatement1(connection, row, col);
+    });
+
+    connection.connect();
+});
+
+
+function executeStatement1(connection, row, col) {
+    const Request = require('tedious').Request
+
+    const sql = `insert into [SharedTalmud].[dbo].[Comments] (ResId, Row, Col) values(1, ${row}, ${col});`
+
+    console.log(sql);
+
+    request = new Request(sql, function(err) {
+        if (err)
+            console.log(JSON.stringify(err));
     });
 
     request.on("requestCompleted", function(rowCount, more) {
