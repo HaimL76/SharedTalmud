0a6b5a481ed78428562b1c60ff9a55dd5163ae24
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-09-13 21:31:35 +0300

    31

--- sortedList.js
+++ sortedList.js
@@ -1,49 +1,66 @@
 class sortedList {
     arr = [];
+    begin = 0;
+    end = 0;
 
     constructor(size) {
-        for (i = 0; i < size; i++)
+        for (let i = 0; i < size; i++)
             this.arr.push(null);
+
+        let middle = size / 2;
+
+        middle = ~~middle;
+
+        this.begin = this.end = middle;
     }
 
-    begin = end = 0;
-
-    add(obj) {
-        this.add(obj, this.begin, this.end)
+    add = (obj) => {
+        if (this.begin === this.end)
+            this.insert(obj, this.begin, this.end < this.arr.length);
+        else
+            this.add0(obj, this.begin, this.end)
     }
 
-    add(obj, b, e) {
-        if (b > this.end) {
-            this[b] = obj;
-        } else if (e < this.begin) {
-            this[e] = obj;
+    add0 = (obj, b, e) => {
+        let middle = (b + e) / 2;
+
+        middle = ~~middle;
+
+        const obj0 = this.arr[middle];
+
+        if (middle == obj0)
+            this.insert(obj, middle, this.end < this.arr.length - 1);
+        else if (obj > obj0)
+            this.add0(obj, middle + 1, e);
+        else if (obj < obj0)
+            this.add0(obj, b, middle - 1);
+    }
+
+    insert = (obj, index, up = null) => {
+        if (up === null)
+            up = this.end < this.arr.length;
+
+        if (up === true) {
+            for (let i = this.end; i > index; i--)
+                this.arr[i] = this.arr[i - 1];
+
+            this.end++;
         } else {
-            middle = (b + e) / 2;
+            for (let i = this.begin; i < index; i++)
+                this.arr[i - 1] = this.arr[i];
 
-            const obj0 = arr[middle];
-
-            if (middle == obj0)
-                this.insert(obj, middle, this.end < this.arr.length - 1);
-            else if (obj > obj0)
-                this.add(obj, index + 1, e);
-            else if (obj < obj0)
-                this.add(obj, b, index - 1);
+            this.begin--;
         }
+
+        this.arr[index] = obj;
     }
 
-    insert(obj, index, up) {
-        if (up) {
-            for (i = end; i > index; i--)
-                arr[i + 1] = arr[i];
+    printConsole = () => {
+        let arr0 = [];
 
-            end++;
-        } else {
-            for (i = begin; i < index; i++)
-                arr[i - 1] = arr[i];
+        for (let i = this.begin; i < this.end; i++)
+            arr0.push(this.arr[i]);
 
-            begin--;
-        }
-
-        arr[index] = obj;
+        console.log(JSON.stringify(arr0));
     }
 }
