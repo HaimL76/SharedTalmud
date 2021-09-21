c66ef501ea739028986610098a44b63ec78e3278
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-09-21 23:35:15 +0300

    74

---
+++ InsertValues.sql
@@ -0,0 +1,4 @@
+insert into [Categories] ([Name]) values(N'תלמוד')
+insert into [Books] ([Name], [Category]) values (N'ברכות', 1)
+insert into [Resources] ([Book], [Page], [Path]) values (1, 1, '1.jpg')
+insert into [Authors] ([First], [Last]) values (N'שלמה', N'זלמן')
