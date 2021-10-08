f6026015931a2a16000e61fe98bafa7341d8fa48
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-10-08 08:42:23 +0300

    140

--- legend.js
+++ legend.js
@@ -1,8 +1,11 @@
 class legendItem {
-    constructor(id, name) {
+    constructor(cb, id, name) {
         this.itemId = id;
         this.itemName = name;
+        this.callback = cb;
     }
+
+    callback = null;
 
     itemId = null;
 
@@ -10,7 +13,14 @@ class legendItem {
 
     itemStyle = null;
 
-    setColor = (color) => this.itemStyle.color = color;
+    setColor = (color) => {
+        if (!this.itemStyle)
+            this.itemStyle = {
+                color: 0
+            };
+
+        this.itemStyle.color = color;
+    }
 
     itemChecked = false;
 
@@ -18,11 +28,15 @@ class legendItem {
 }
 
 class legend {
-    constructor(element, url) {
+    constructor(cb, element, url) {
         this.myElement = element;
 
         this.theUrl = url;
+
+        this.callback = cb;
     }
+
+    callback = null;
 
     items = null; //new Map();
 
@@ -33,6 +47,8 @@ class legend {
     buildItems = async() => {
         const items0 = new Map();
 
+        const cb = this.callback;
+
         await $.ajax({
             type: "GET",
             url: this.theUrl,
@@ -42,7 +58,7 @@ class legend {
                         const itemId = arr.Id;
                         const itemName = arr.Name;
 
-                        const item = new legendItem(itemId, itemName);
+                        const item = new legendItem(cb, itemId, itemName);
 
                         items0.set(itemId, item);
                     });
@@ -53,24 +69,65 @@ class legend {
         return items0;
     }
 
+    getColorItem = (itemId) => {
+        if (itemId > 0 && this.items && this.items.has(itemId))
+            return this.items.get(itemId);
+    }
+
     build = async() => {
+        const cb = this.callback;
+
         this.items = await this.buildItems();
 
-        this.items.forEach(pair => {
+        const myItems = this.items;
+
+        myItems.forEach(item => {
             let str = "";
 
-            str += "<ul>";
+            str += "<ul class=aks>";
             str += "<li>";
-            str += pair.getCheckbox();
+            str += item.getCheckbox();
 
+            const itemId = `color_${item.itemId}`;
 
-            str += `<input type="color" id="color_${pair.itemId}" onchange="clickColor(0, -1, -1, 5)" value="#ff0000"></input>`
+            str += `<input type="color" id="${itemId}" value="#ff0000"></input>`
 
+            str += item.itemName;
 
             str += "</li>";
             str += "</ul>";
 
             $(`#${this.myElement}`).append(str);
+
+            //$('input[type=color]').on('input',
+            $(`#${itemId}`).on('input', (e) => {
+                const elem0 = e.target;
+
+                const color0 = elem0.value;
+
+                const itemId = elem0.id;
+
+                if (itemId) {
+                    const arr = itemId.split("_");
+
+                    if (Array.isArray(arr) && arr.length == 2 && arr[1]) {
+                        const id = parseInt(arr[1]);
+
+                        if (this.items.has(id)) {
+                            const item0 = this.items.get(id);
+
+                            if (item0) {
+                                item0.setColor(color0);
+
+                                console.log(color0);
+
+                                if (cb)
+                                    cb(color0);
+                            }
+                        }
+                    }
+                }
+            });
         });
     }
 }
