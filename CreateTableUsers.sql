1071662d4b8f51520ce9eed95afef02f90bd9c3a
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-10-15 01:24:59 +0300

    152

--- CreateTableUsers.sql
+++ CreateTableUsers.sql
@@ -8,6 +8,9 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+IF EXISTS(SELECT * FROM sys.indexes WHERE  [name] = 'UC_Username')
+	DROP INDEX [UC_Username] ON [dbo].[Users]
+
 IF EXISTS(SELECT * FROM sys.tables WHERE [type] = 'U' and [name] = 'Users')
 	drop table [dbo].[Users]
 
@@ -25,11 +28,20 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+
+
 ALTER TABLE [dbo].[Users]  WITH CHECK ADD FOREIGN KEY([AuthorId])
 REFERENCES [dbo].[Authors] ([Id])
 GO
 
 ALTER TABLE [dbo].[Users] ADD  DEFAULT (getdate()) FOR [Timestamp]
 GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [UC_Username] ON [dbo].[Users]
+(
+	[Username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
 
 
